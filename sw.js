720ce51bb2508a92abc0bd336f041eade261e40c
Kaczonator2137 <oskaridamian1234@gmail.com>
2026-08-26 18:44:25 +0200

    Refresh PWA cache for expanded library

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='calislevel-v6-production-copy';
+const CACHE='calislevel-v7-expanded-library';
 const CORE=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
